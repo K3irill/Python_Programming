b389de9c927c48b98d32f1db9a949668ad373a51
Kirill <razdor.2019@mail.ru>
2024-10-13 23:45:27 +0300

    learning py

--- tasks.py
+++ tasks.py
@@ -15,3 +15,34 @@ money = 100
 print(f"{money - (cost * weightProduct):.0f}")
 
 print(f"{money - (2.5 * 38):.0f}")
+
+# 
+def string_to_number(s):
+    return int(s)
+# 
+def dna_to_rna(dna):
+        result = list()
+        for molecule in list(dna):
+              if molecule == "T":
+                    result.append('U')
+              else: result.append(molecule)
+        return result							
+              
+print(dna_to_rna("GCAT"))
+
+arr = ['G', 'C', 'A', 'T', 'T', 'G', 'T']
+
+result = [item for item in arr if item == 'T']
+# new_arr = ['X' if item == 'T' else item for item in arr] сразу с заменой
+print(result)
+
+numbers = [-5, -4, -3 ,-2, -1, 0, 1, 2, 3, 4, 5]
+result2 = filter(lambda n: n % 2 == 0 and n != 0, numbers)
+
+for n in result2: print(n, end=' ')
+
+result3 = map(lambda item: 'U' if item == 'T' else item, arr)
+newArr = list()
+for n in result3: newArr.append(n)
+readyStr = ''.join(newArr)
+print(readyStr)
